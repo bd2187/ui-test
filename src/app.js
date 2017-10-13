e8ea557e0784bfc13d4c031fd23cf4d9484befab
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-13 12:37:28 -0700

    Add color module which is responsible for change color logic

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 var buttonModule = function() {
+  // Select button elements
   var counterButton = document.querySelector('.counter');
   var resetButton = document.querySelector('.reset');
 
-  function counter() {
+  function counterEvent() {
     return counterButton.addEventListener('click', () => {
+      // Add one to counter button's number
       counterButton.textContent++;
 
+      // if button's text content is 5, disable counter button
       if (counterButton.textContent == 5) {
         counterButton.disabled = true;
       }
     });
   }
 
-  function reset() {
+  function resetEvent() {
+    // reset counter button
     return resetButton.addEventListener('click', () => {
       counterButton.textContent = 0;
       counterButton.disabled = false;
@@ -20,12 +24,47 @@ var buttonModule = function() {
   }
 
   return {
-    reset,
-    counter
+    resetEvent,
+    counterEvent
   };
 };
 
+var colorModule = function() {
+  // Select first column
+  var columnOne = document.querySelector('#col-1');
+
+  const colorEvent = () => {
+    /*
+      Add event listener to columnOne and invoke changeColor with the
+      event as its argument
+    */
+    return columnOne.addEventListener('click', event => changeColor(event));
+  };
+
+  function changeColor(n) {
+    const colorText = n.target;
+    const colorID = n.target.id;
+
+    /*
+      If element's text color is equal to the element's id,
+      change text color to black, otherwise, utilize colorID
+      as text color.
+    */
+    return colorText.style.color === colorID
+      ? (colorText.style.color = '#000')
+      : (colorText.style.color = colorID);
+  }
+
+  return { colorEvent };
+};
+
+// Invoke events
 const buttons = buttonModule();
-const { reset, counter } = buttons;
-counter();
-reset();
+const colors = colorModule();
+
+const { colorEvent } = colors;
+const { resetEvent, counterEvent } = buttons;
+
+counterEvent();
+resetEvent();
+colorEvent();
